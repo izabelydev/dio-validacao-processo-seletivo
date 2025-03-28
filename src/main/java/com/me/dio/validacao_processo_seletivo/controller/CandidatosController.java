325d600f8738611ac963c4226e86d0f7dd68575f
izabelydev <izabelylrnc@gmail.com>
2025-03-28 00:04:07 -0300

    Anotacao para gerar construtor da controller

--- src/main/java/com/me/dio/validacao_processo_seletivo/controller/CandidatosController.java
+++ src/main/java/com/me/dio/validacao_processo_seletivo/controller/CandidatosController.java
@@ -1,20 +1,21 @@
 package com.me.dio.validacao_processo_seletivo.controller;
 
 import com.me.dio.validacao_processo_seletivo.data.InclusaoCandidatos;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@AllArgsConstructor
 public class CandidatosController {
+    /**
+     * @AllArgsConstructor
+     * Injeção de dependência pelo construtor para que a classe inclusaoCandidatos instancie e inicie automaticamente.
+     */
 
     private final InclusaoCandidatos inclusaoCandidatos;
-
-    // Injeção de dependência pelo construtor para que a inclusao de candidatos instancie e inicie automaticamente
-    public CandidatosController(InclusaoCandidatos inclusaoCandidatos) {
-        this.inclusaoCandidatos = inclusaoCandidatos;
-    }
 
     @GetMapping("/candidatos-cadastrados")
     public Object candidatos() {
